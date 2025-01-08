0b13d4782c488f6c61a44cd9b1e9b2177d21d804
madhat5 <davidmalary@gmail.com>
2025-01-07 19:23:10 -0500

    feat: add imports, setup data for chart

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+import Sankey from '../components/Sankey'
+
+import data from '../data/data.json'
+
 function App() {
-  const [count, setCount] = useState(0)
+  console.log('data', data)
+
+  const nodes = [];
+  const links = [];
+
+  const nodeMap = new Map();
+
+  const addNode = (name) => {
+    if (!nodeMap.has(name)) {
+      const index = nodes.length;
+      nodeMap.set(name, index);
+      nodes.push({ node: index, name });
+    }
+    return nodeMap.get(name);
+  };
+
+  data.forEach((entry) => {
+    const catCodeIndex = addNode(entry.cat_code);
+    const systemStepName = `Step ${entry.system_step} (${entry.category})`; 
+    const systemStepIndex = addNode(systemStepName);
+    const systemCountry = `${entry.system} - ${entry.country}`
+    // const systemIndex = addNode(entry.system);
+    const systemIndex = addNode(systemCountry);
+  
+    // Create links
+    links.push({ source: catCodeIndex, target: systemStepIndex, value: 1 });
+    links.push({ source: systemStepIndex, target: systemIndex, value: 1 });
+  });
+
+  const result = { nodes, links };
+  console.log(result);
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Sankey />
     </>
   )
 }
