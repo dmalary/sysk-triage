4e345cb15a479b052b0adbb71343b43d6a5763ea
madhat5 <davidmalary@gmail.com>
2025-01-07 19:22:29 -0500

    feat: base component

---
+++ components/Sankey.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable react/prop-types */
+
+import * as d3 from 'd3';
+
+const Sankey = ({width, height, data}) => {
+
+  return (
+    <div>
+      hello world
+    </div>
+  )
+}
+export default Sankey;
